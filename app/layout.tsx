a2fd38dd10c25a01d17301443a3c9ed6dcf7f811
Mika Siddiqui <36973525+mikasiddiqui@users.noreply.github.com>
2026-03-07 01:18:09 +1100

    latent foundry

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,52 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const silkaMono = localFont({
+  variable: "--font-silka-mono",
+  display: "swap",
+  src: [
+    {
+      path: "../public/fonts/silkamono-thin-webfont.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/silkamono-extralight-webfont.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/silkamono-light-webfont.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/silkamono-regular-webfont.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/silkamono-medium-webfont.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/silkamono-semibold-webfont.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/silkamono-bold-webfont.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/silkamono-black-webfont.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -24,11 +61,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${silkaMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
